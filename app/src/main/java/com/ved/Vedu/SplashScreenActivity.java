4929b2f739ea0e07db1281b1621d3e040eb4bd2c
Vedprakash Lilhare <vedprakashlilhare2002@gmail.com>
2024-03-03 00:10:47 +0530

    splash screen

---
+++ app/src/main/java/com/ved/Vedu/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.ved.Vedu;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ved.mysafety.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar();
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+        },1000);
+    }
+}
