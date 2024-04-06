eb7af5faffe42c8030705b685be92460d553d75b
Vedprakash Lilhare <vedprakashlilhare2002@gmail.com>
2024-04-06 18:24:39 +0530

    learn form video

--- app/src/main/java/com/ved/Vedu/MainActivity.java
+++ app/src/main/java/com/ved/Vedu/MainActivity.java
@@ -1,7 +1,5 @@
 package com.ved.Vedu;
 
-import static android.content.ContentValues.TAG;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,21 +15,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.ved.mysafety.R;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     CardView c1,c2;
     private DatabaseReference mDatabase;
-    ImageView l1,l2,l3;
     private FirebaseAuth mAuth;
-
+    ImageView l1,l2,l3;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         c1 = findViewById(R.id.v1);
-        c2 = findViewById(R.id.v2);
+      //  c2 = findViewById(R.id.v2);
         l1 =findViewById(R.id.b1);
         l2 =findViewById(R.id.b2);
         l3 =findViewById(R.id.b3);
@@ -50,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        c2.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this,SoS.class);
-            startActivity(intent);
-        });
+//        c2.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this,SoS.class);
+//            startActivity(intent);
+//        });
 
         l1.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this,number.class);
@@ -69,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this,near.class);
             startActivity(intent);
         });
-
         mDatabase.child("org").child("Notice").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -82,15 +73,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-    public String dateconverter(String string){
-        long unixTime = Long.parseLong(string); // Convert string to long
-
-        Date date = new Date(unixTime * 1000L); // Convert Unix time to milliseconds
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
-        String formattedDate = sdf.format(date);
-        return formattedDate;
 
     }
     public void signout(View view){
