9d86de7aef716747e2c627c520c7101f1f26d5ad
Harsh Tiwari <htiwari702@gamil.com>
2024-04-06 20:06:41 +0530

    add new user

--- app/src/main/java/com/ved/Vedu/admin.java
+++ app/src/main/java/com/ved/Vedu/admin.java
@@ -1,6 +1,8 @@
 package com.ved.Vedu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,31 @@ import com.ved.mysafety.R;
 
 public class admin extends AppCompatActivity {
 
+    Button a1,a2,a3,a4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        a1 =findViewById(R.id.add_user_button);
+//        a2 =findViewById(R.id.upload_video_button);
+//        a3 =findViewById(R.id.upload_notification_button);
+//        a4 =findViewById(R.id.ph);
+        a1.setOnClickListener(v -> {
+            Intent intent = new Intent(admin.this, SignupActivity.class);
+            startActivity(intent);
         });
+//        a2.setOnClickListener(v -> {
+//            Intent intent = new Intent(admin.this,SignupActivity.class);
+//            startActivity(intent);
+//        });
+//        a3.setOnClickListener(v -> {
+//            Intent intent = new Intent(admin.this,SignupActivity.class);
+//            startActivity(intent);
+//        });
+//        a4.setOnClickListener(v -> {
+//            Intent intent = new Intent(admin.this,SignupActivity.class);
+//            startActivity(intent);
+//        });
     }
 }
